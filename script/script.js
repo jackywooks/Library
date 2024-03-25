8d10248d4719c63f4d3ecf616476ca34318ab84e
Jacky Woo <146035892+jackywooks@users.noreply.github.com>
2024-03-25 00:11:07 -0400

    Add Dialog HTML and event handler

--- script/script.js
+++ script/script.js
@@ -1,31 +1,72 @@
 const addButton = document.querySelector("#addBook");
+const dialog = document.querySelector("dialog");
+const confirmButton = dialog.querySelector("#submit");
+const inputArray = dialog.querySelectorAll("input");
 
 //Event Listeners
-addButton.addEventListener("click", () => createDiv());
+// "Show the dialog" button opens the dialog modally
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+});
 
+
+// "Close" button closes the dialog
+confirmButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary(inputArray[0].value,inputArray[1].value,inputArray[2].value,inputArray[3].value)
+  displayBooks();
+  dialog.close();
+});
 
 const myLibrary = [];
 
 class Book {
-  constructor(title, pages, description) {
+  constructor(title, author, pages, remark) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
-    this.description = description;
+    this.remark = remark;
   }
-
-  addBookToLibrary(title, pages, description) {}
 }
 
-function createDiv() {
+function addBookToLibrary(title, author, pages, remark) {
+  const newBook = new Book(title, author, pages, remark);
+  myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  for (const book of myLibrary) {
+    createCard(book.title, book.author, book.pages, book.remark);
+  }
+}
+
+function popUpForm() {
+}
+
+function createCard(title, author, pages, remark) {
+  //create Book Card
   const main = document.querySelector("main");
   const card = document.createElement("div");
   card.setAttribute("class", "bookCard");
   main.appendChild(card);
 
+  //close button
   const closeBtn = document.createElement("button");
   closeBtn.textContent = "x";
   card.appendChild(closeBtn);
   closeBtn.addEventListener("click", () => card.parentNode.removeChild(card));
 
-
+  //Content
+  const bookTitle = document.createElement("p");
+  bookTitle.textContent = `Title: ${title}`;
+  card.appendChild(bookTitle);
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = `Author: ${author}`;
+  card.appendChild(bookAuthor);
+  const bookPages = document.createElement("p");
+  bookPages.textContent = `Pages: ${pages}`;
+  card.appendChild(bookPages);
+  const bookRemark = document.createElement("p");
+  bookRemark.textContent = `Remark: ${remark}`;
+  card.appendChild(bookRemark);
 }
