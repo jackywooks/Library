8de7a2ba5c1608592538229a7974960e3d2c12ab
Jacky Woo <146035892+jackywooks@users.noreply.github.com>
2024-03-24 22:43:57 -0400

    Add Create BookCard Div Function

--- script/script.js
+++ script/script.js
@@ -1,15 +1,31 @@
+const addButton = document.querySelector("#addBook");
+
+//Event Listeners
+addButton.addEventListener("click", () => createDiv());
+
+
 const myLibrary = [];
 
-function Book(title,pages,description) {
-  this.title = title;
-  this.pages = pages;
-  this.description = description;
+class Book {
+  constructor(title, pages, description) {
+    this.title = title;
+    this.pages = pages;
+    this.description = description;
+  }
+
+  addBookToLibrary(title, pages, description) {}
 }
 
+function createDiv() {
+  const main = document.querySelector("main");
+  const card = document.createElement("div");
+  card.setAttribute("class", "bookCard");
+  main.appendChild(card);
+
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "x";
+  card.appendChild(closeBtn);
+  closeBtn.addEventListener("click", () => card.parentNode.removeChild(card));
 
 
-
-function addBookToLibrary() {
-  // do stuff here
 }
-
